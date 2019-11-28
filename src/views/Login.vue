ef7b454f72421721a77ddf5fd40ee1eb4fcd41fa
Mohamad Lutfi <mohamadlutfi00@gmail.com>
2019-11-28 12:57:51 +0700

    Update base

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,80 +4,238 @@
 }
 </style>
 <template>
-	<div>
-		<div class="login-box">
-			<div class="login-logo animated fadeInUp">
-				<a href="../../index2.html"><b>Canopus</b>UI</a>
-			</div>
-			<!-- /.login-logo -->
-			<div class="login-box-body animated fadeInUp">
-				<p class="login-box-msg">Sign in to start your session</p>
-				<div v-if="isLoginError" class="alert alert-warning alert-dismissable">
-				<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-				<strong>Warning!</strong> {{messageErrorLogin}} </div>
-
-				<form v-on:submit.prevent="onSubmit(username, password);" >
-					<div class="form-group has-feedback">
-						<input v-model="username" class="form-control" placeholder="Username">
-						<span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-					</div>
-					<div class="form-group has-feedback">
-						<input type="password" v-model="password" class="form-control" placeholder="Password">
-						<span class="glyphicon glyphicon-lock form-control-feedback"></span>
-					</div>
-					<div class="row">
-						<div class="col-xs-8">
-							<div class="checkbox icheck">
-								<label>
-									<input type="checkbox"> Remember Me
-								</label>
-							</div>
-						</div>
-						<!-- /.col -->
-						<div class="col-xs-4">
-							<button id="login" type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-							<!-- <div id="login-loading" class="lds-ring" style="display:none"><div></div><div></div><div></div><div></div></div> -->
-							<div id="login-loading" style="display:none" class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
-						</div>
-						<!-- /.col -->
-					</div>
-				</form>
-
-				<a href="#">I forgot my password</a><br>
-
-			</div>
-			<!-- /.login-box-body -->
-		</div>
-	</div>
+  <div class="m-grid m-grid--hor m-grid--root m-page">
+    <div
+      class="m-grid__item m-grid__item--fluid m-grid m-grid--hor m-login m-login--signin m-login--2 m-login-2--skin-2"
+      id="m_login"
+      style="background-image: url(../../../assets/app/media/img//bg/bg-3.jpg);"
+    >
+      <div class="m-grid__item m-grid__item--fluid	m-login__wrapper">
+        <div class="m-login__container">
+          <div class="m-login__logo">
+            <a href="#">
+              <!--
+                <img src="../../../assets/app/media/img//logos/logo-1.png">
+              -->
+            </a>
+          </div>
+          <div class="m-login__signin">
+            <div class="m-login__head">
+              <h3 class="m-login__title">Sign In To Admin</h3>
+            </div>
+            <div
+              v-if="isLoginError"
+              class="alert alert-warning alert-dismissable"
+            >
+              <button
+                type="button"
+                class="close"
+                data-dismiss="alert"
+                aria-hidden="true"
+              >
+                ×
+              </button>
+              <strong>Warning!</strong> {{ messageErrorLogin }}
+            </div>
+            <form
+              v-on:submit.prevent="onSubmit(username, password);"
+              class="m-login__form m-form"
+            >
+              <div class="form-group m-form__group">
+                <input
+                  v-model="username"
+                  class="form-control m-input"
+                  type="text"
+                  placeholder="Username"
+                />
+              </div>
+              <div class="form-group m-form__group">
+                <input
+                  v-model="password"
+                  type="password"
+                  class="form-control m-input m-login__form-input--last"
+                  placeholder="Password"
+                />
+              </div>
+              <div class="row m-login__form-sub">
+                <div class="col m--align-left m-login__form-left">
+                  <label class="m-checkbox  m-checkbox--focus">
+                    <input type="checkbox" name="remember" /> Remember me
+                    <span></span>
+                  </label>
+                </div>
+                <div class="col m--align-right m-login__form-right">
+                  <a
+                    href="javascript:;"
+                    id="m_login_forget_password"
+                    class="m-link"
+                  >
+                    Forget Password ?
+                  </a>
+                </div>
+              </div>
+              <div class="m-login__form-action">
+                <button
+                  id="m_login_signin_submit"
+                  class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air m-login__btn m-login__btn--primary"
+                >
+                  Sign In
+                </button>
+              </div>
+            </form>
+          </div>
+          <div class="m-login__signup">
+            <div class="m-login__head">
+              <h3 class="m-login__title">Sign Up</h3>
+              <div class="m-login__desc">
+                Enter your details to create your account:
+              </div>
+            </div>
+            <form class="m-login__form m-form" action="">
+              <div class="form-group m-form__group">
+                <input
+                  class="form-control m-input"
+                  type="text"
+                  placeholder="Fullname"
+                  name="fullname"
+                />
+              </div>
+              <div class="form-group m-form__group">
+                <input
+                  class="form-control m-input"
+                  type="text"
+                  placeholder="Email"
+                  name="email"
+                  autocomplete="off"
+                />
+              </div>
+              <div class="form-group m-form__group">
+                <input
+                  class="form-control m-input"
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                />
+              </div>
+              <div class="form-group m-form__group">
+                <input
+                  class="form-control m-input m-login__form-input--last"
+                  type="password"
+                  placeholder="Confirm Password"
+                  name="rpassword"
+                />
+              </div>
+              <div class="row form-group m-form__group m-login__form-sub">
+                <div class="col m--align-left">
+                  <label class="m-checkbox m-checkbox--focus">
+                    <input type="checkbox" name="agree" /> I Agree the
+                    <a href="#" class="m-link m-link--focus">
+                      terms and conditions
+                    </a>
+                    . <span></span>
+                  </label>
+                  <span class="m-form__help"></span>
+                </div>
+              </div>
+              <div class="m-login__form-action">
+                <button
+                  id="m_login_signup_submit"
+                  class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air  m-login__btn"
+                >
+                  Sign Up
+                </button>
+                &nbsp;&nbsp;
+                <button
+                  id="m_login_signup_cancel"
+                  class="btn btn-outline-focus m-btn m-btn--pill m-btn--custom  m-login__btn"
+                >
+                  Cancel
+                </button>
+              </div>
+            </form>
+          </div>
+          <div class="m-login__forget-password">
+            <div class="m-login__head">
+              <h3 class="m-login__title">Forgotten Password ?</h3>
+              <div class="m-login__desc">
+                Enter your email to reset your password:
+              </div>
+            </div>
+            <form class="m-login__form m-form" action="">
+              <div class="form-group m-form__group">
+                <input
+                  class="form-control m-input"
+                  type="text"
+                  placeholder="Email"
+                  name="email"
+                  id="m_email"
+                  autocomplete="off"
+                />
+              </div>
+              <div class="m-login__form-action">
+                <button
+                  id="m_login_forget_password_submit"
+                  class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air  m-login__btn m-login__btn--primaryr"
+                >
+                  Request
+                </button>
+                &nbsp;&nbsp;
+                <button
+                  id="m_login_forget_password_cancel"
+                  class="btn btn-outline-focus m-btn m-btn--pill m-btn--custom m-login__btn"
+                >
+                  Cancel
+                </button>
+              </div>
+            </form>
+          </div>
+          <div class="m-login__account">
+            <span class="m-login__account-msg">
+              Don't have an account yet ?
+            </span>
+            &nbsp;&nbsp;
+            <a
+              href="javascript:;"
+              id="m_login_signup"
+              class="m-link m-link--light m-login__account-link"
+            >
+              Sign Up
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import { LOGIN } from "@/store/actions.type";
 import { mapGetters } from "vuex";
 
 export default {
-	name: "Login",
-	data() {
-		return {
-			username: null,
-			password: null
-		};
-	},
-	methods: {
-    onSubmit(username, password) {
-			$("#login").hide();
-			$("#login-loading").show();
-			this.$store
-				.dispatch(LOGIN, { username, password })
-				.then(() =>{ 
-				window.location.href = "/";
-			}).catch(() => {
-				$("#login").show();
-				$("#login-loading").hide();
-			});
-    }
-	},
-	computed: {
-    ...mapGetters(["messageErrorLogin","isLoginError"])
+  name: "Login",
+  data() {
+    return {
+      username: null,
+      password: null
+    };
   },
-}
+  methods: {
+    onSubmit(username, password) {
+      $("#login").hide();
+      $("#login-loading").show();
+      this.$store
+        .dispatch(LOGIN, { username, password })
+        .then(() => {
+          window.location.href = "/";
+        })
+        .catch(() => {
+          $("#login").show();
+          $("#login-loading").hide();
+        });
+    }
+  },
+  computed: {
+    ...mapGetters(["messageErrorLogin", "isLoginError"])
+  }
+};
 </script>
